9b20f90620770e4242ed842d5981dfe5cd668578
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-09-14 16:51:22 +0300

    Add search to admin UI.

--- property/admin.py
+++ property/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Flat
 
 
-admin.site.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    list_display = ('town', 'address', 'owner', 'price', 'rooms_number', 'active')
+    search_fields = ['town', 'address', 'owner']
+    list_per_page = 15
+
+
+admin.site.register(Flat, FlatAdmin)
