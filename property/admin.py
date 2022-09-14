015e6e83f0629a4e1e9b4bb16fe2b702ff38fb0d
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-09-14 17:11:51 +0300

    Customize Flat Admin.

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,8 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'address', 'owner', 'price', 'rooms_number', 'active')
     list_per_page = 15
-    readonly_fields = ['created_at',]
+    readonly_fields = ['created_at', ]
     search_fields = ['town', 'address', 'owner']
+
 
 admin.site.register(Flat, FlatAdmin)
